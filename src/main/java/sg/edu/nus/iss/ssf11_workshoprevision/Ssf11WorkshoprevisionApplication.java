e8de6d00c2df20b8bbfa379a86f15bd5402e76b2
antsimch <anthonysimch@gmail.com>
2023-06-04 21:06:22 +0800

    task 1, 2, 3

--- src/main/java/sg/edu/nus/iss/ssf11_workshoprevision/Ssf11WorkshoprevisionApplication.java
+++ src/main/java/sg/edu/nus/iss/ssf11_workshoprevision/Ssf11WorkshoprevisionApplication.java
@@ -1,13 +1,37 @@
 package sg.edu.nus.iss.ssf11_workshoprevision;
 
+import java.util.Collections;
+import java.util.List;
+
+import org.springframework.boot.DefaultApplicationArguments;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Ssf11WorkshoprevisionApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Ssf11WorkshoprevisionApplication.class, args);
-	}
+	private static final String DEFAULT_PORT = "3000";
 
+	public static void main(String[] args) {
+
+		SpringApplication app = new SpringApplication(Ssf11WorkshoprevisionApplication.class);
+		DefaultApplicationArguments appArgs = new DefaultApplicationArguments(args);
+
+		List<String> opsValues = appArgs.getOptionValues("port");
+		String port;
+
+		if (opsValues == null || opsValues.get(0) == null) {
+			port = System.getenv("PORT");
+
+			if (port == null) 
+				port = DEFAULT_PORT;
+		} else {
+			port = opsValues.get(0);
+		}
+
+		if (port != null) 
+			app.setDefaultProperties(Collections.singletonMap("server.port", port));
+
+		app.run(args);
+	}
 }
